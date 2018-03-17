e5b2033a7ee3a6632d3b7213984105b01c23b1c9
Артём Коновалов <arkonline@yandex.ru>
2018-03-17 14:11:11 +0300

    Создал проект, разделил View и BL. Начал описывать FileManager в BL

---
+++ Jobs.BL/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobs.BL
+{
+    public class FileManager
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+
+        public string GetContent(string filepath)
+        {
+            return GetContent(filepath, _defaultEncoding);
+        }
+
+        public string GetContent(string filePath, Encoding encoding )
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;//
+        }
+    }
+}
