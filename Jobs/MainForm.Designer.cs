8d728c552a011a841091c2ab541089aabaff8eb9
Артём Коновалов <arkonline@yandex.ru>
2018-03-17 15:31:47 +0300

    Переименована форма

--- Jobs/MainForm.Designer.cs
+++ Jobs/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Jobs
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 660);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Jobs/Program.cs
+++ Jobs/Program.cs
@@ -16,7 +16,7 @@ namespace Jobs
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
