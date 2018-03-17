22bc6b6626296e61a7a110389dfca5c7d8a61aea
Артём Коновалов <arkonline@yandex.ru>
2018-03-17 17:51:23 +0300

    Ветвь BL слита с ветвью View. Создан конструктор класса MainPresenter

--- Jobs/MainPresenter.cs
+++ Jobs/MainPresenter.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jobs.BL;
 
 namespace Jobs
 {
     class MainPresenter
     {
+        private readonly IMainForm _view;
+        private readonly IFileManager _manager;
+        private readonly IMessageService _messageService;
+
+        public MainPresenter(IMainForm viev, IFileManager manager, IMessageService service)
+        {
+            _view = viev;
+            _manager = manager;
+            _messageService = service;
+        }
     }
 }
