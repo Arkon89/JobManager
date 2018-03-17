069ede36e170f764b53ad6ea2d3cafb76e7f7832
Артём Коновалов <arkonline@yandex.ru>
2018-03-17 15:56:52 +0300

    настроены элементы формы

--- Jobs/MainForm.Designer.cs
+++ Jobs/MainForm.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.fldNewJob = new System.Windows.Forms.TextBox();
+            this.btAddJob = new System.Windows.Forms.Button();
+            this.btDeleteJob = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.HorizontalScrollbar = true;
+            this.listBoxMain.ItemHeight = 16;
+            this.listBoxMain.Location = new System.Drawing.Point(12, 120);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(441, 532);
+            this.listBoxMain.TabIndex = 0;
+            // 
+            // fldNewJob
+            // 
+            this.fldNewJob.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fldNewJob.Location = new System.Drawing.Point(12, 32);
+            this.fldNewJob.Name = "fldNewJob";
+            this.fldNewJob.Size = new System.Drawing.Size(441, 22);
+            this.fldNewJob.TabIndex = 1;
+            // 
+            // btAddJob
+            // 
+            this.btAddJob.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btAddJob.Location = new System.Drawing.Point(475, 32);
+            this.btAddJob.Name = "btAddJob";
+            this.btAddJob.Size = new System.Drawing.Size(94, 23);
+            this.btAddJob.TabIndex = 2;
+            this.btAddJob.Text = "Добавить ";
+            this.btAddJob.UseVisualStyleBackColor = true;
+            // 
+            // btDeleteJob
+            // 
+            this.btDeleteJob.Location = new System.Drawing.Point(475, 120);
+            this.btDeleteJob.Name = "btDeleteJob";
+            this.btDeleteJob.Size = new System.Drawing.Size(94, 23);
+            this.btDeleteJob.TabIndex = 3;
+            this.btDeleteJob.Text = "Удалить";
+            this.btDeleteJob.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 660);
+            this.Controls.Add(this.btDeleteJob);
+            this.Controls.Add(this.btAddJob);
+            this.Controls.Add(this.fldNewJob);
+            this.Controls.Add(this.listBoxMain);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Список дел";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxMain;
+        private System.Windows.Forms.TextBox fldNewJob;
+        private System.Windows.Forms.Button btAddJob;
+        private System.Windows.Forms.Button btDeleteJob;
     }
 }
 
