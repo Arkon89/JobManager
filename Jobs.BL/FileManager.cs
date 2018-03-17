817c6e10d4e2450018ebc88d9e73c74a95e647e1
Артём Коновалов <arkonline@yandex.ru>
2018-03-17 15:27:53 +0300

    заполнен класс FileManager

--- Jobs.BL/FileManager.cs
+++ Jobs.BL/FileManager.cs
@@ -11,6 +11,12 @@ namespace Jobs.BL
     {
         private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
 
+        public bool IsExist(string filePath)
+        {
+            bool isExist = File.Exists(filePath);
+            return isExist;
+        }
+
         public string GetContent(string filepath)
         {
             return GetContent(filepath, _defaultEncoding);
@@ -21,5 +27,17 @@ namespace Jobs.BL
             string content = File.ReadAllText(filePath, encoding);
             return content;//
         }
+
+        public void SaveContent(string content ,string filepath)
+        {
+            SaveContent(content ,filepath, _defaultEncoding);
+        }
+
+        public void SaveContent(string content ,string filePath, Encoding encoding)
+        {
+            File.WriteAllText(filePath, content, encoding);
+            
+        }
+
     }
 }
